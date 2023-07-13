2fc4f94924be2e9bc8f77bc4119270f82a80f4eb
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-13 08:28:00 -0700

    Done

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include"unistd"
+/**
+ * main - Entry point
+ * Return: 1 (not success)
+ */
+int main(void)
+{
+	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	write(1, quote, 59);
+	return (1);
+}
