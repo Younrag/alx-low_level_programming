fb46554f2e233352be6c33c7fd7273fed4b20344
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-18 10:47:21 +0000

    Done

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times the alphabet, in lowercase
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+
+	n = 0;
+	while (n < 10)
+	{
+		c = 'a'
+		while (c <= 'z')
+			_putchar(c++);
+		_putchar('\n');
+	}
+}
