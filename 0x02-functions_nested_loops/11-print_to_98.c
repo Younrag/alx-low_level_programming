9aa1f315bca1d4d6fdc86690c4cef19ea928012c
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-18 15:24:56 +0000

    Done

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print all natural numbers from n to 98
+ * @n: the start number
+ */
+void print_to_98(int n)
+{
+	int q, r;
+
+	while (n < 99)
+	{
+		if (n < 10)
+		{
+		_putchar('0' + n);
+		_putchar(',');
+		_putchar(' ');
+		}
+		else if (n != 98)
+		{
+			q = n / 10;
+			r = n % 10;
+			_putchar(q);
+			_putchar(r);
+			_putchar(',');
+			_putchar(' ');
+		}
+		else
+		{
+			_putchar(9);
+			_putchar(8);
+		}
+		n++;
+	}
+	_putchar('\n');
+}
