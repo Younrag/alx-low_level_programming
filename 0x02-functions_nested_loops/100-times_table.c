cb781642df14f3ef47fa4d19f9818b0a19c7682d
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-18 16:48:20 +0000

    Done

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,8 +1,47 @@
 #include "main.h"
 
 /**
- * 
+ * print_times_table - prints the n times table, starting with 0
+ * @n: input
  */
 void print_times_table(int n)
 {
+	int p, q;
+
+	p = 0;
+	if (n <= 0 || n >= 15)
+		break;
+	while (p <= n)
+	{
+		_putchar(48);
+		q = 1;
+		while (q <= n)
+		{
+			prod = p * q;
+			_putchar(',');
+			_putchar(' ');
+			if (prod < 10)
+			{
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(prod + 48);
+			}
+			else if (prod < 100)
+			{
+				_putchar(' ');
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			else
+			{
+				_putchar((prod / 100) + 48);
+				_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
+			}
+			q++;
+		}
+		p++;
+	}
+	_putchar('\n');
+}
 
