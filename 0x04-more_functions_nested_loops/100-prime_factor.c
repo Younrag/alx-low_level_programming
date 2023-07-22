959b7d025bff6f5a6efcf419a166ae9d1fd6fb6e
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-21 18:58:41 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: largest prime
+ * Return: 0
+ */
+int main(void)
+{
+	long int i, p;
+	long int a = 612852475143;
+
+	p = 0;
+	for (i = 2; i <= a; i++)
+	{
+		if ((a % i) == 0)
+		{
+			a = a / i;
+			p = i;
+		}
+	}
+	printf("The largest prime is: %ld", p);
+}
