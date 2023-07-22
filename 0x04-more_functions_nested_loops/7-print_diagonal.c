8bbf415b8ffbaa856c18c3867deb4f8a4653cbd6
Younrag <younes.raguebi@usmba.ac.ma>
2023-07-21 17:15:45 -0700

    Done

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a diagonal lines
+ * @n: number of times the character / should be printed
+ */
+void print_diagonal(int n)
+{
+	if (n > 0)
+	{
+		int i, j;
+
+		for (j = 1; j <= n; j++)
+		{
+			for (i = 1; i < j; i++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+}
