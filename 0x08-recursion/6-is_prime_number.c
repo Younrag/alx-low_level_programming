3a08d4fe5a74c3246bbbf1fc135f8df36ea85047
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-02 19:08:04 +0000

    Done

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -13,7 +13,7 @@ int test_prime_number(int n, int i)
 		return (1);
 	if (n % i == 0)
 		return (0);
-	return (test_prime_number(n. i + 1));
+	return (test_prime_number(n, i + 1));
 }
 /**
  * is_prime_number - says if an integer is a prime number or not
