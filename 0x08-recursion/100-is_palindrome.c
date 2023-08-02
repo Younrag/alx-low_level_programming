eea87a85d6d78fcfcbe189da5e5761e9d1028e8f
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-02 19:16:52 +0000

    Done

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -7,10 +7,10 @@
 
 int _strlen_recursion(char *s)
 {
-        if (*s)
-                return (1 + _strlen_recursion(s + 1));
-        else
-                return (0);
+	if (*s)
+		return (1 + _strlen_recursion(s + 1));
+	else
+		return (0);
 }
 /**
  * check_palindrome - checks the characters recursively for palindrome
