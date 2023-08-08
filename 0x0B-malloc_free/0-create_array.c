7cf691afd3c41019fec764e7310b6a99ead0f12a
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-08 03:06:18 -0700

    Done

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -10,11 +10,12 @@
 char *create_array(unsigned int size, char c)
 {
 	char *s;
+	unsigned int i;
 
 	if (size == 0)
 		return (NULL);
 	s = malloc(sizeof(unsigned int)* size);
-	for (unsigned int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 		s[i] = c;
 	return (s);
 }
