9a5da3170c98c37f5b3ca781e60258a56aba29a2
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-11 10:04:09 -0700

    Done

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,7 +1,8 @@
 #include "main.h"
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h> 
+#include <string.h>
+#include <ctype.h>
 /**
  * main - multiplies two positive numbers
  * @argc: number of arguments
@@ -15,7 +16,7 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
+	if (argc != 3 || !isdigit(s1) || !isdigit(s2))
 	{
 		printf("Error\n");
 		exit(98);
@@ -33,7 +34,7 @@ int main(int argc, char *argv[])
 	{
 		digit1 = s1[len1] - '0';
 		carry = 0;
-		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
+		for (len2 = strlen(s2) - 1; len2 >= 0; len2--)
 		{
 			digit2 = s2[len2] - '0';
 			carry += result[len1 + len2 + 1] + (digit1 * digit2);
