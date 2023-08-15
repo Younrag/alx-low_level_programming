66dea7f3cb0aab4addefbf5a9c903d7f83348e2f
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-15 10:20:22 -0700

    Done

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ * print_dog - prints a struct dog
+ * @d: struct dog to print
+ */
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+		printf("Name: (nil)\n");
+	else if (d->age == NULL || d->owner == NULL)
+		printf("(nil)");
+	else if (d)
+		printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+}
