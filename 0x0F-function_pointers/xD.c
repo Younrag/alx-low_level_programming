7c5d2a53acb98f5c514397f6f3ee3b567a725ca3
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-16 17:46:08 +0000

    Done

--- 0x0F-function_pointers/xD.c
+++ 0x0F-function_pointers/xD.c
@@ -1,18 +1,18 @@
 #include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
 #include "3-calc.h"
 /**
- * main - Entry main
- * Description: calcule with five operators
+ * main - Prints the result of simple operations.
  * @argc: The number of arguments supplied to the program.
  * @argv: An array of pointers to the arguments.
  *
- * Return: 0
+ * Return: Always 0.
  */
-
 int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
-	char *operator;
+	char *op;
 
 	if (argc != 4)
 	{
@@ -21,24 +21,23 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 	}
 
 	num1 = atoi(argv[1]);
+	op = argv[2];
 	num2 = atoi(argv[3]);
-	operator = argv[2];
 
-	if ((*argv[2] != '+' &&
-			*argv[2] != '-' &&
-			*argv[2] != '*' &&
-			*argv[2] != '/' &&
-			*argv[2] != '%') ||
-			operator[1] != '\0')
+	if (get_op_func(op) == NULL || op[1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*argv[2] == '/' || *argv[2] == '%') && (num2 == 0))
+
+	if ((*op == '/' && num2 == 0) ||
+	    (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
 	}
-	printf("%d\n", get_op_func(operator)(num1, num2));
+
+	printf("%d\n", get_op_func(op)(num1, num2));
+
 	return (0);
 }
