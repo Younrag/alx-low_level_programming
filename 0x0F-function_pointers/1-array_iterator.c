334b65704d582d0ffafebb1960dd347c2f2fa170
Younrag <younes.raguebi@usmba.ac.ma>
2023-08-16 07:20:15 -0700

    Done

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -7,9 +7,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	size_t i;
+
 	if (action == NULL)
 		return;
-	for (int i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
 		action(array[i]);
 	}
