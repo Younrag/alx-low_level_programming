dda098a7e4b81a44a0aa52f0ce6b2655ca7e3aea
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-04 22:35:30 +0000

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,9 +10,13 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
-	ssize_t wr = 0, len;
+	ssize_t wr = 0, len = 0;
 
-	len = strlen(text_content);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 	if (!filename)
 		return (-1);
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0600);
