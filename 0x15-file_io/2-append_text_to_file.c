f59e23122012771c032f5cb0420b4ae7f1b8dfd8
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-04 22:24:24 +0000

    Done

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -11,12 +11,16 @@
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int o, w, len;
+	int o, w, len = 0;
 
 	if (filename == NULL)
 		return (-1);
 
-	len = strlen(text_content);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
 
 	o = open(filename, O_WRONLY | O_APPEND);
 	w = write(o, text_content, len);
