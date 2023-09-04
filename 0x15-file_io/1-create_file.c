72cc073f690060d8f04c34a606552f3854999b5b
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-04 22:01:16 +0000

    Done

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -13,7 +13,7 @@ int create_file(const char *filename, char *text_content)
 
 	if (!filename)
 		return (-1);
-	fd = open(filename, O_RDWR | O_CREAT, 0600 | O_TRUNC);
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	wr = write(fd, text_content, strlen(text_content));
 	if (wr == -1 || fd == -1)
 		return (-1);
