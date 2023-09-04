d4084f73e817c49df82a2743a89141d587114dbc
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-04 13:58:45 +0000

    Done

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -13,7 +13,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	char *buffer;
 
 	fd = open(filename, O_RDONLY);
-	if (fd == -1 || !filename || !letters)
+	if (fd == -1 || !filename)
 		return (0);
 	buffer = malloc(letters * sizeof(char));
 	rd = read(fd, buffer, letters);
