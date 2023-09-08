bb323a240e9ee884394d7ed3ea8c1333622b34f0
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-08 18:12:05 +0000

    Done

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,13 +1,13 @@
 #include "main.h"
 
 /**
- * Entry main - copie content
+ * main - copie content
  * @argc: number of argument
  * @argv: array of pointers to arguments
- * Description: program that copies the content of a file to another file.
  * Return: 0 on succes
+ *
+ * Description: program that copies the content of a file to another file.
  */
-
 int main(int argc, char **argv)
 {
 	int fd1, fd2, rd, wr;
@@ -25,12 +25,12 @@ int main(int argc, char **argv)
 		exit(99);
 	}
 
-	fd1 = open(*argv[1], O_RDONLY);
-	fd2 = open(*argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	fd1 = open(argv[1], O_RDONLY);
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	rd = read(fd1, buffer, 1024);
 	if (fd1 == -1 || rd == -1)
 		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	wr = (fd2, buffer, rd);
+	wr = write(fd2, buffer, rd);
 	if (fd2 == -1 || wr == -1)
 		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
 	fd1 = close(fd1);
