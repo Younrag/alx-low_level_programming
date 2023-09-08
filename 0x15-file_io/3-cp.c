f58b722be41fd4b4d35251944ee037e05fd66dd1
Younrag <younes.raguebi@usmba.ac.ma>
2023-09-08 18:05:46 +0000

    Done

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * Entry main - copie content
+ * @argc: number of argument
+ * @argv: array of pointers to arguments
+ * Description: program that copies the content of a file to another file.
+ * Return: 0 on succes
+ */
+
+int main(int argc, char **argv)
+{
+	int fd1, fd2, rd, wr;
+	char *buffer;
+
+	if (argc != 3)
+	{
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	buffer = malloc(1024 * sizeof(char));
+	if (buffer == NULL)
+	{
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+
+	fd1 = open(*argv[1], O_RDONLY);
+	fd2 = open(*argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	rd = read(fd1, buffer, 1024);
+	if (fd1 == -1 || rd == -1)
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]), exit(98);
+	wr = (fd2, buffer, rd);
+	if (fd2 == -1 || wr == -1)
+		dprintf(2, "Error: Can't write to %s\n", argv[2]), exit(99);
+	fd1 = close(fd1);
+	fd2 = close(fd2);
+	if (fd1 == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd1), exit(100);
+	if (fd2 == -1)
+		dprintf(2, "Error: Can't close fd %d\n", fd2), exit(100);
+
+	return (0);
+}
