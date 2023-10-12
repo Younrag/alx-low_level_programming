d33cd33217f9b67a448a222509952182854596ea
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-12 20:07:41 +0100

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,7 +11,7 @@
 size_t print_dlistint(const dlistint_t *h)
 {
         dlistint_t *current;
-        size_t count = 0;
+        int count = 0;
 
         current = h;
         if (current == NULL)
