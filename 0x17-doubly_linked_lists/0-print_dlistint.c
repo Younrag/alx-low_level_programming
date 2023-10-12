47e607e09acab3a5c8f38359f58f10445fc4f98e
Younrag <younes.raguebi@usmba.ac.ma>
2023-10-12 20:12:11 +0100

    Done

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -16,14 +16,14 @@ size_t print_dlistint(const dlistint_t *h)
 	if (h == NULL)
 		return (count);
 
-	while (h->prev != NULL)
+	while (h->prev)
 		h = h->prev;
 
-	while (h != NULL)
+	while (h)
 	{
 		printf("%d\n", h->n);
-		count++;
 		h = h->next;
+		count++;
 	}
 
 	return (count);
