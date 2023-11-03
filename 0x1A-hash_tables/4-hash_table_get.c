13f2e6156b0b40ee4d3004131e4a1178e85368f9
Younrag <younes.raguebi@usmba.ac.ma>
2023-11-03 18:36:00 +0100

    Done

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,7 +10,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index = 0;
 	hash_node_t *Head;
 
-	if (key == '\0' || key == NULL || ht == NULL)
+	if (*key == '\0' || key == NULL || ht == NULL)
 		return (NULL);
 	index = key_index((unsigned char *)key, (*ht).size);
 	Head = (*ht).array[index];
