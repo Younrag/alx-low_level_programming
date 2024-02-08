72abc3884262d35f02c70bcce9aaaee0ede89192
Younrag <younes.raguebi@usmba.ac.ma>
2024-02-08 21:05:32 +0100

    Done

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -19,9 +19,10 @@ int linear_search(int *array, size_t size, int value)
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%ld] = [%d]", i, array[i]);
 		if (array[i] == value)
 			return (i);
 	}
+
 	return (-1);
 }
