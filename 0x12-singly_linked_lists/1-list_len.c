6c60390ca28ea844c7d08680487f9c01e14990c1
Younrag <younes.raguebi@usmba.ac.ma>
2025-03-25 11:53:36 +0000

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,18 +2,17 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a
- * linked list_t list.
+ * list_len - returns the number of elements in a linked list_t list.
  * @h: pointer to a linked list_t list.
- * 
+ *
  * Return: number of nodes.
  */
- 
+
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
-	
-	while(h)
+
+	while (h)
 	{
 		h = h->next;
 		n++;
