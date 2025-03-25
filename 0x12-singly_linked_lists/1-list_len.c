7570ea3cbb1f8d5626c68e9c52569f182db67505
Younrag <younes.raguebi@usmba.ac.ma>
2025-03-25 11:51:08 +0000

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -9,10 +9,12 @@
  * Return: number of nodes.
  */
  
-size_t list_len(const list_t *h){
+size_t list_len(const list_t *h)
+{
 	size_t n = 0;
 	
-	while(h){
+	while(h)
+	{
 		h = h->next;
 		n++;
 	}
