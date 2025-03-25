d7994dbfbb95e8174f713981351e90d4ee70b0a1
Younrag <younes.raguebi@usmba.ac.ma>
2025-03-25 11:49:48 +0000

    Done

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -2,7 +2,8 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list_t list.
+ * list_len - returns the number of elements in a
+ * linked list_t list.
  * @h: pointer to a linked list_t list.
  * 
  * Return: number of nodes.
